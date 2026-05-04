7e3ccf09d541c36b390ebeebcf2c6e46c80fa4e4
kk04205 <hs1208@scarletmail.rutgers.edu>
2026-05-04 14:35:03 -0400

    Update CSV file path in load_staging.sql

--- sql/load_staging.sql
+++ sql/load_staging.sql
@@ -1,5 +1,5 @@
 -- Load the raw CSV file into the staging table 
 COPY staging_diabetes_raw
-FROM '/Users/kk/Desktop/archive/diabetes_binary_health_indicators_BRFSS2015.csv'  
+FROM 'diabetes_binary_health_indicators_BRFSS2015.csv'  
 DELIMITER ','
 CSV HEADER;
