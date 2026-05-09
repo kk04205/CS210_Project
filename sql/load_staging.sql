6377a70bd857595a3a8293f81b724930a5c866b1
kk04205 <hs1208@scarletmail.rutgers.edu>
2026-05-08 22:54:28 -0400

    Simplify copy command for loading CSV into staging

--- sql/load_staging.sql
+++ sql/load_staging.sql
@@ -1,5 +1,1 @@
--- Load the raw CSV file into the staging table 
-\copy staging_diabetes_raw
-FROM 'diabetes_binary_health_indicators_BRFSS2015.csv'
-DELIMITER ','
-CSV HEADER;
+\copy staging_diabetes_raw FROM 'diabetes_binary_health_indicators_BRFSS2015.csv' DELIMITER ',' CSV HEADER;
