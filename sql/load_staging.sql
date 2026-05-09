83175705c6e8eef928a889677290e817c6c29993
kk04205 <hs1208@scarletmail.rutgers.edu>
2026-05-08 22:41:40 -0400

    Update COPY command to \\copy for staging load

--- sql/load_staging.sql
+++ sql/load_staging.sql
@@ -1,5 +1,5 @@
 -- Load the raw CSV file into the staging table 
-COPY staging_diabetes_raw
-FROM 'diabetes_binary_health_indicators_BRFSS2015.csv'  
+\copy staging_diabetes_raw
+FROM 'diabetes_binary_health_indicators_BRFSS2015.csv'
 DELIMITER ','
 CSV HEADER;
